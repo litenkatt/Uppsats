319c46c65dc7f6cc1e25dce5fd114949c13da426
Rasmus <rasmus.yt@gmail.com>
2018-05-28 23:15:28 +0200

    Updated for logic module

--- test_scenarios.py
+++ test_scenarios.py
@@ -2,6 +2,7 @@ import test_data
 import qlearning
 import clustering_qlearning
 import clustering_dsl
+import logic_qlearning
 from timeit import timeit
 from timeit import default_timer as timer
 from datetime import datetime as dt
@@ -37,12 +38,12 @@ test_pairs = [
 
 ###################################################################################################
 
-### LOGIC/CONTEXT SENSING DIVERSITY
+### Clustering
 
 test_agents = test_data.sensor_agents
-test_contexts = test_data.contexts
+test_contexts = test_data.test_contexts
 
-logic_test_pairs = [
+clustering_test_pairs = [
     [
         [
             {
@@ -76,6 +77,63 @@ logic_test_pairs = [
         }
     ],
 ]
+
+###################################################################################################
+
+### LOGIC
+
+ls_1 = test_data.logic_sensors_s1
+
+logic_test_pairs = [
+    [
+        [
+            [
+                [
+                    'phone',
+                    'pos'
+                ],
+                ls_1['phone']['pos']
+            ],
+            0,
+            [
+                [
+                    'temp',
+                    'living_room'
+                ],
+                ls_1['temp']['living_room']
+            ],
+            21
+        ],
+        {
+                'heater' : True
+        }
+    ],
+    [
+        [
+            [
+                [
+                    'phone',
+                    'pos'
+                ],
+                ls_1['phone']['pos']
+            ],
+            0,
+            [
+                [
+                    'temp',
+                    'living_room'
+                ],
+                25
+            ],
+            21
+        ],
+        {
+                'cooler' : True
+        }
+    ],
+]
+
+
 
 ###################################################################################################
 
@@ -134,14 +192,14 @@ def test_interoperability():
 def test_clustering():
     clustering_dsl.sensor_agents = test_agents
     ql = clustering_qlearning
-    for i in range(len(logic_test_pairs)):
+    for i in range(len(clustering_test_pairs)):
         print('Test[' + str(i + 1) + ']:')
-        ql.train(logic_test_pairs[i][0], logic_test_pairs[i][1], 5000, ql.num_methods - 1)
+        ql.train(clustering_test_pairs[i][0], clustering_test_pairs[i][1], 5000, ql.num_methods - 1)
         print()
 
     print('Testing programs:')
     print()
-    for t in logic_test_pairs:
+    for t in clustering_test_pairs:
         print('Input:')
         print('\t' + str(t[0]))
 
@@ -182,7 +240,41 @@ def test_clustering():
 #    except TypeError:
 #        print('No program found')
 
+def test_logic():
+    ql = logic_qlearning
+    for i in range(len(logic_test_pairs)):
+        print('Test[' + str(i + 1) + ']:')
+        ql.train(logic_test_pairs[i][0], logic_test_pairs[i][1], 5000, ql.num_methods)
+        print()
+
+    print('Testing programs:')
+    print()
+    for t in test_data.logic_search_data:
+        for k, v in  test_data.actuators.items():
+            v = False
+        print('Input:')
+        print('\t' + str(t))
+        print('\t' + str(test_data.actuators))
+
+        program = ql.search(t)
+
+        if program == None:
+            print('\tProgram failure\n')
+            continue
+
+        print('Result:')
+        for r in ql.run(t):
+            print('\t' + str(r))
+        print('Program(s) used:')
+        for p in program:
+            print('\t' + str(p) + ', ')
+        print(str(len(program)) + ' program(s)')
+        print('Time:')
+        print('\t' + str("%.2f" % (ql.time_search(t, 5000) * 100000)) + 'us')
+        print()
+
 #test_interoperability()
 #test_clustering()
+test_logic()
 
 ###################################################################################################
