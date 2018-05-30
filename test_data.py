0acad2353981c77caf47cda44d8f6196be36a9dd
Rasmus <rasmus.yt@gmail.com>
2018-05-30 09:40:09 +0200

    Offloaded test data to the test_data file.

--- test_data.py
+++ test_data.py
@@ -1,6 +1,39 @@
 from datetime import datetime as dt
 
-sensor_agents = {
+### INTEROPERABILITY
+
+interop_test_pairs = [
+    [   # 1: gmqtt without mid-> MQTT Standard
+        ['PUBLISH', False, 0, False, -1, -1, 'test/mqtt_standard/1', -1, 'property1', 'property2', 'Test payload'],
+        ['PUBLISH', False, 0, False, -1, -1, 'test/mqtt_standard/1', [-1, 'property1', 'property2', 'Test payload']]
+    ],
+    [   # 2: gmqtt with mid -> MQTT Standard
+        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/2', 1234, -1, 'property1', 'property2', 'Test payload'],
+        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/2', 1234, [-1, 'property1', 'property2', 'Test payload']]
+    ],
+    [   # 3: Eclipse Paho Python -> MQTT Standard
+        {'command':'PUBLISH', 'mid':1234, 'qos':0, 'pos':0, 'to_process':-1, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/3', 1234, 'Test payload'], 'info':'property1'},
+        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/3', 1234, 'Test payload']
+    ],
+    [   # 4: Eclipse Paho Python -> gmqtt
+        {'command':'PUBLISH', 'mid':1234, 'qos':0, 'pos':0, 'to_process':-1, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/gmqtt/1', 1234, 'Test payload'], 'info':'property1'},
+        ['PUBLISH', False, 1, False, -1, -1, 'test/gmqtt/1', 1234, [-1, 'property1', 'Test payload']]
+    ],
+    [   # 5: MQTT Standard -> Eclipse Paho Python
+        ['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'],
+        {'command':'PUBLISH', 'mid':1234, 'qos':1, 'pos':0, 'to_process':9, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'], 'info':''}
+    ],
+    [   # 6: gmqtt -> Eclipse Paho Python
+        ['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, -1, 'property1', 'Test payload'],
+        {'command':'PUBLISH', 'mid':1234, 'qos':1, 'pos':0, 'to_process':9, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'], 'info':[-1, 'property1']}
+    ],
+]
+
+###################################################################################################
+
+### Clustering
+
+clustering_sensors = {
     'sensor1' : {
         'temp' : 20.3,
         'time' : dt(2018, 5, 20, 10, 0, 0),
@@ -18,7 +51,7 @@ sensor_agents = {
     }
 }
 
-test_contexts = {
+clustering_contexts = {
     'c1' : [
             'loc',
             {
@@ -48,19 +81,97 @@ test_contexts = {
     ],
 }
 
+clustering_test_pairs = [
+    [
+        [
+            {
+                'sensor101' : {
+                    'temp' : 26.4,
+                    'time' : dt(2018, 5, 20, 10, 0, 0),
+                    'loc' : 'Kista'
+                }
+            },
+            clustering_contexts
+        ],
+        {
+            'c1': [
+                'loc',
+                {
+                    'loc': [2, 0.0, 'Kista'],
+                    'temp': [2, 3.049999999999999, 23.35],
+                    'time': [2, 18000.0, dt(2018, 5, 20, 5, 0)]
+                },
+                ['sensor1', 'sensor101']
+            ],
+            'c3': [
+                'time',
+                {
+                    'loc': [3, 2.309401076758503, 'Kista'],
+                    'temp': [3, 2.734755727462488, 22.133333333333336],
+                    'time': [3, 0.0, dt(2018, 5, 20, 10, 0)],
+                },
+                ['sensor1', 'sensor2', 'sensor101']
+            ],
+        }
+    ],
+]
+
+###################################################################################################
+
+### LOGIC
+
+logic_test_pairs = [
+    [
+        [
+            [
+                [
+                    'phone',
+                    'pos'
+                ],
+                1000
+            ],
+            0,
+            [
+                [
+                    'temp',
+                    'living_room'
+                ],
+                17
+            ],
+            21
+        ],
+        {
+                'heater' : True
+        }
+    ],
+    [
+        [
+            [
+                [
+                    'phone',
+                    'pos'
+                ],
+                1000
+            ],
+            0,
+            [
+                [
+                    'temp',
+                    'living_room'
+                ],
+                25
+            ],
+            21
+        ],
+        {
+                'cooler' : True
+        }
+    ],
+]
 
 actuators = {
     'heater' : False,
     'cooler' : False,
-}
-
-logic_sensors_s1 = {
-    'phone' : {
-        'pos' : 1000,
-    },
-    'temp' : {
-        'living_room' : 17,
-    }
 }
 
 logic_search_data = [
@@ -105,3 +216,5 @@ logic_search_data = [
         ],
     ],
 ]
+
+###################################################################################################
--- test_scenarios.py
+++ test_scenarios.py
@@ -1,146 +1,18 @@
 import test_data
-import qlearning
-import clustering_qlearning
+import interoperability
+import clustering
 import clustering_dsl
-import logic_qlearning
+import logic
 from timeit import timeit
 from timeit import default_timer as timer
 from datetime import datetime as dt
 
-### INSTANTIATION/INTEROPERABILITY
-
-test_pairs = [
-    [   # 1: gmqtt without mid-> MQTT Standard
-        ['PUBLISH', False, 0, False, -1, -1, 'test/mqtt_standard/1', -1, 'property1', 'property2', 'Test payload'],
-        ['PUBLISH', False, 0, False, -1, -1, 'test/mqtt_standard/1', [-1, 'property1', 'property2', 'Test payload']]
-    ],
-    [   # 2: gmqtt with mid -> MQTT Standard
-        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/2', 1234, -1, 'property1', 'property2', 'Test payload'],
-        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/2', 1234, [-1, 'property1', 'property2', 'Test payload']]
-    ],
-    [   # 3: Eclipse Paho Python -> MQTT Standard
-        {'command':'PUBLISH', 'mid':1234, 'qos':0, 'pos':0, 'to_process':-1, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/3', 1234, 'Test payload'], 'info':'property1'},
-        ['PUBLISH', False, 1, False, -1, -1, 'test/mqtt_standard/3', 1234, 'Test payload']
-    ],
-    [   # 4: Eclipse Paho Python -> gmqtt
-        {'command':'PUBLISH', 'mid':1234, 'qos':0, 'pos':0, 'to_process':-1, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/gmqtt/1', 1234, 'Test payload'], 'info':'property1'},
-        ['PUBLISH', False, 1, False, -1, -1, 'test/gmqtt/1', 1234, [-1, 'property1', 'Test payload']]
-    ],
-    [   # 5: MQTT Standard -> Eclipse Paho Python
-        ['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'],
-        {'command':'PUBLISH', 'mid':1234, 'qos':1, 'pos':0, 'to_process':9, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'], 'info':''}
-    ],
-    [   # 6: gmqtt -> Eclipse Paho Python
-        ['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, -1, 'property1', 'Test payload'],
-        {'command':'PUBLISH', 'mid':1234, 'qos':1, 'pos':0, 'to_process':9, 'packet':['PUBLISH', False, 1, False, -1, -1, 'test/eclipse_paho_python/1', 1234, 'Test payload'], 'info':[-1, 'property1']}
-    ],
-]
-
-###################################################################################################
-
-### Clustering
-
-test_agents = test_data.sensor_agents
-test_contexts = test_data.test_contexts
-
-clustering_test_pairs = [
-    [
-        [
-            {
-                'sensor101' : {
-                    'temp' : 26.4,
-                    'time' : dt(2018, 5, 20, 10, 0, 0),
-                    'loc' : 'Kista'
-                }
-            },
-            test_contexts
-        ],
-        {
-            'c1': [
-                'loc',
-                {
-                    'loc': [2, 0.0, 'Kista'],
-                    'temp': [2, 3.049999999999999, 23.35],
-                    'time': [2, 18000.0, dt(2018, 5, 20, 5, 0)]
-                },
-                ['sensor1', 'sensor101']
-            ],
-            'c3': [
-                'time',
-                {
-                    'loc': [3, 2.309401076758503, 'Kista'],
-                    'temp': [3, 2.734755727462488, 22.133333333333336],
-                    'time': [3, 0.0, dt(2018, 5, 20, 10, 0)],
-                },
-                ['sensor1', 'sensor2', 'sensor101']
-            ],
-        }
-    ],
-]
-
-###################################################################################################
-
-### LOGIC
-
-ls_1 = test_data.logic_sensors_s1
-
-logic_test_pairs = [
-    [
-        [
-            [
-                [
-                    'phone',
-                    'pos'
-                ],
-                ls_1['phone']['pos']
-            ],
-            0,
-            [
-                [
-                    'temp',
-                    'living_room'
-                ],
-                ls_1['temp']['living_room']
-            ],
-            21
-        ],
-        {
-                'heater' : True
-        }
-    ],
-    [
-        [
-            [
-                [
-                    'phone',
-                    'pos'
-                ],
-                ls_1['phone']['pos']
-            ],
-            0,
-            [
-                [
-                    'temp',
-                    'living_room'
-                ],
-                25
-            ],
-            21
-        ],
-        {
-                'cooler' : True
-        }
-    ],
-]
-
-
-
-###################################################################################################
-
 ### RUN TESTS
 
 def test_interoperability():
-    ql = qlearning
+    test_pairs = test_data.interop_test_pairs
+    ql = interoperability
+
     for i in range(len(test_pairs)):
         print('Test[' + str(i + 1) + ']:')
         ql.train(test_pairs[i][0], test_pairs[i][1], 5000, ql.num_methods - 1)
@@ -190,16 +62,20 @@ def test_interoperability():
         print('No program found')
 
 def test_clustering():
+    test_agents = test_data.clustering_sensors
+    test_contexts = test_data.clustering_contexts
+    test_pairs = test_data.clustering_test_pairs
     clustering_dsl.sensor_agents = test_agents
-    ql = clustering_qlearning
-    for i in range(len(clustering_test_pairs)):
+    ql = clustering
+
+    for i in range(len(test_pairs)):
         print('Test[' + str(i + 1) + ']:')
-        ql.train(clustering_test_pairs[i][0], clustering_test_pairs[i][1], 5000, ql.num_methods - 1)
+        ql.train(test_pairs[i][0], test_pairs[i][1], 5000, ql.num_methods - 1)
         print()
 
     print('Testing programs:')
     print()
-    for t in clustering_test_pairs:
+    for t in test_pairs:
         print('Input:')
         print('\t' + str(t[0]))
 
@@ -241,10 +117,11 @@ def test_clustering():
 #        print('No program found')
 
 def test_logic():
-    ql = logic_qlearning
-    for i in range(len(logic_test_pairs)):
+    test_pairs = test_data.logic_test_pairs
+    ql = logic
+    for i in range(len(test_pairs)):
         print('Test[' + str(i + 1) + ']:')
-        ql.train(logic_test_pairs[i][0], logic_test_pairs[i][1], 5000, ql.num_methods)
+        ql.train(test_pairs[i][0], test_pairs[i][1], 5000, ql.num_methods)
         print()
 
     print('Testing programs:')
@@ -273,8 +150,8 @@ def test_logic():
         print('\t' + str("%.2f" % (ql.time_search(t, 5000) * 100000)) + 'us')
         print()
 
-#test_interoperability()
-#test_clustering()
+test_interoperability()
+test_clustering()
 test_logic()
 
 ###################################################################################################
